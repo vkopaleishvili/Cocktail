9f81779b7a728cffa40d8708a428130662f19456
Val Copala <val@superteamusa.com>
2023-08-19 20:46:17 -0400

    version2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import bodyparser from "body-parser";
 import axios from "axios";
 
@@ -14,40 +14,68 @@ app.listen(port, ()=> {
 
 let urlRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 let urlName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+let url = urlRandom;
+var cocktails;
+var count = 0;
 
 app.get("/", async (req,res) => {
     try 
     {
-const result = await axios.get(urlRandom);
-const drink = result.data.drinks[0];
-// console.log(drink);
-        res.render("index.ejs", {
-name: drink.strDrink,
-instructions:  drink.strInstructions,
-ingredient: [drink.strIngredient1, drink.strIngredient2, drink.strIngredient3, drink.strIngredient4, drink.strIngredient5], 
-        });
+const result = await axios.get(url);
+cocktails = result.data.drinks;
+const drink = cocktails[count];
+    res.render("index.ejs", {
+        drinkImg: drink.strDrinkThumb,
+        name: drink.strDrink,
+        instructions:  drink.strInstructions,
+        ingredient: [drink.strIngredient1, drink.strIngredient2, drink.strIngredient3, drink.strIngredient4, drink.strIngredient5], 
+    });
     } catch (error) {
         // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-      }
-    
+      } 
 })
 
-app.post("/submit", async (req,res) => {
-    try 
-    {
-        console.log("start");
-        const url = urlName + req.body.coctailName;
-        console.log("URL", url);
-const result = await axios.get(url);
-const drink = result.data.drinks[0];
-// console.log(drink);
-res.render("index.ejs", {
-    name: drink.strDrink,
-    instructions:  drink.strInstructions,
-    ingredient: [drink.strIngredient1, drink.strIngredient2, drink.strIngredient3, drink.strIngredient4, drink.strIngredient5], 
-            });
-    } catch (error) {
-        // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-      }
-    
+
+
+
+app.post("/getrecipe", async (req,res) => {
+        url = urlName + req.body.coctailName;
+        count = 0;
+res.redirect("/");
+})
+
+app.post("/findCocktailbyLetter", (req,res) => {
+        url = urlName + req.body.strDrinkLetter.charAt(0);
+        count = 0;
+res.redirect("/");
+})
+
+app.post("/findCockTailByIngredient", (req,res) => {
+    url = urlName + req.body.strIngredient;
+    count = 0;
+res.redirect("/");
+})
+
+
+
+app.post("/nextItem", (req,res) => {
+        if (count < cocktails.length) {
+            count = count + 1;
+            console.log(count);
+} if (count === cocktails.length) {
+    count = 0;
+    count = count + 1;
+}
+res.redirect("/");
+})
+
+app.post("/previousItem", (req,res) => {
+    if (count > 0) {
+        count = count - 1;
+        console.log(count);
+} if (count === 0) {
+count = cocktails.length;
+count = count - 1;
+}
+res.redirect("/");
 })
